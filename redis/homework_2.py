27f27f658e805e730f7cf8253efa53371f3c5c71
kkkiss <38090414+kkkiss@users.noreply.github.com>
2018-04-11 20:21:36 -0400

    finish homework 2

--- redis/homework_2.py
+++ redis/homework_2.py
@@ -15,8 +15,17 @@ def article_vote(redis, user, article):
             reids.hincrby(article, 'votes', 1)
 
 def article_switch_vote(redis, user, from_article, to_article):
-    # HOMEWORK 2 Part I
-    pass
+    from_article_id = from_article.split(':')[-1]
+    to_article_id = to_article.split(':')[-1]
+   
+    redis.zincrby(name = 'score:', value = from_article, amount = VOTE_SCORE*(-1))
+    redis.hincrby(name = from_article, key='votes', amount = -1)
+    redis.srem('voted:' + from_article_id, user)
+
+    redis.zincrby(name = 'score:', value = to_article, amount = VOTE_SCORE)
+    redis.hincrby(name = to_article, key ='votes', amount = 1)
+    redis.sadd('voted:' + to_article_id, user)
+
 
 redis = redis.StrictRedis(host='localhost', port=6379, db=0)
 # user:3 up votes article:1
@@ -26,9 +35,10 @@ article_vote(redis, "user:3", "article:3")
 # user:5 switches their vote from article:1 to article:0
 article_switch_vote(redis, "user:2", "article:8", "article:1")
 
-# Which article's score is between 10 and 20?
-# PRINT THE ARTICLE'S LINK TO STDOUT:
-# HOMEWORK 2 Part II
-# article = redis.?
-# print redis.?
+
+article = redis.zrangebyscore('score:', 10, 20)
+if len(article) > 1:
+    result = article[0]
+article_id = result[0].split(':')[-1]
+print(redis.hget("article:" +  article_id, "link"))
 
